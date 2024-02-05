1f8ac32a9a0185bcb50218d869a5c04a4fa5d060
Jessica Noya <jessicapnoya@gmail.com>
2024-02-04 23:35:26 -0500

    add style.css

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Select elements from DOM
 const form = document.querySelector('#todo-form')
 const input = document.querySelector('#user-input')
 const list = document.querySelector('#list')
@@ -13,13 +14,19 @@ form.addEventListener('submit', function(event) {
         removeBtn.innerText = 'Remove'
         list.append(newTodo)
         newTodo.appendChild(removeBtn)
+        // Set input to an empty string, this clears the input box
+        input.value = ''
     }
 })
 
 list.addEventListener('click', function(event) {
+    // If button is clicked on, remove it
     if (event.target.tagName === 'BUTTON') {
         event.target.parentElement.remove()
     }
 
-    
+    // If element is clicked on, strike through with CSS selector
+    if (event.target.tagName === 'LI') {
+        event.target.classList.add('strike')
+    }
 })
