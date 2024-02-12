3d0575cb9f1bc75bc6a7ec641d2ab93b88a7a259
Jessica Noya <jessicapnoya@gmail.com>
2024-02-11 19:14:49 -0500

    Added localStorage

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
+// 1. Store items in local storage to an array
+// 2. Get items from local storage and remove from array
+// 3. Retrieve elements from local storage and render list elements
+// TODO: Make it less redundant 
+
 // Select elements from DOM
 const form = document.querySelector('#todo-form')
 const input = document.querySelector('#user-input')
 const list = document.querySelector('#list')
+
+document.addEventListener('DOMContentLoaded', function() {
+    let existingTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    for (let item of existingTodos){
+        addMarkupToDOM(item)
+    }
+})
 
 // Submit event fires when a <form> is submitted
 form.addEventListener('submit', function(event) {
     // In a form element, when the user submits the page automatically refreshes. This prevents that from happening
     event.preventDefault()
 
-    // Create element list
-    const newTodo = document.createElement('li')
-    // Create element button
-    const removeBtn = document.createElement('button')
-
     // If value is not an empty string, do something
     if (input.value !== '') {
-        // .value retrieves the current value of an input field in a form
-        // innerText sets the newTodo value
-        newTodo.innerText = input.value
-        // Set innerText of button element to 'Remove'
-        removeBtn.innerText = 'Remove'
-        // Append list item to unordered list
-        list.append(newTodo)
-        // Append the element as the last child of an element, in other words, to the li element
-        newTodo.appendChild(removeBtn)
-        // Set input to an empty string, this clears the input box
+
+        let existingTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+        existingTodos.push(input.value);
+        localStorage.setItem("todos", JSON.stringify(existingTodos));
+        addMarkupToDOM(input.value)
+       
         input.value = ''
     }
 })
@@ -33,6 +37,9 @@ form.addEventListener('submit', function(event) {
 list.addEventListener('click', function(event) {
     // If button is clicked on, remove it
     if (event.target.tagName === 'BUTTON') {
+        let existingTodos = JSON.parse(localStorage.getItem("todos")) || [];
+        const updatedTodos = removeItemFromArray(existingTodos, event.target.parentElement.innerText)
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
         event.target.parentElement.remove()
     }
 
@@ -41,3 +48,31 @@ list.addEventListener('click', function(event) {
         event.target.classList.add('strike')
     }
 })
+
+function removeItemFromArray(arr, todo) {
+    let result = []
+
+    for (let item of arr) {
+        // Why is it adding 'Remove' to the end of the todo string?
+        let addRemoveString = item + 'Remove'
+        if (addRemoveString !== todo) {
+            result.push(item)
+        }
+    }
+    return result
+}
+
+function addMarkupToDOM(input){
+    const newTodo = document.createElement('li')
+    // Create element button
+    const removeBtn = document.createElement('button')
+    // .value retrieves the current value of an input field in a form
+    // innerText sets the newTodo value
+    newTodo.innerText = input
+    // Set innerText of button element to 'Remove'
+    removeBtn.innerText = 'Remove'
+    // Append list item to unordered list
+    list.append(newTodo)
+    // Append the element as the last child of an element, in other words, to the li element
+    newTodo.appendChild(removeBtn)
+}
